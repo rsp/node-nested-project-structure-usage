25c775162ed0d5f65166e562468dae38eb40859e
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-09-30 16:05:52 +0200

    update tests

--- test2.js
+++ test2.js
@@ -1,3 +1,5 @@
-var {moduleA} = require('nested-project-structure-example/dist/moduleA');
-console.error('test2:', moduleA.info);
+var t = 'test2:', module = 'nested-project-structure-example/dist/moduleA';
+console.error(t, 'importing', module);
+var {moduleA} = require(module);
+console.error(t, moduleA.info);
 process.exit(moduleA.info ? 0 : 1);
--- test3.js
+++ test3.js
@@ -1,3 +1,5 @@
-var {moduleA} = require('nested-project-structure-example/moduleA');
-console.error('test3:', moduleA.info);
+var t = 'test3:', module = 'nested-project-structure-example/moduleA';
+console.error(t, 'importing', module);
+var {moduleA} = require(module);
+console.error(t, moduleA.info);
 process.exit(moduleA.info ? 0 : 1);
